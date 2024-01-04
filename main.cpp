68d2d828fb86807d8231e0f9ddc65fdc6dadd4e4
dlamei <44316365+dlamei@users.noreply.github.com>
2024-01-04 17:43:31 +0100

    init project

---
+++ main.cpp
@@ -0,0 +1,179 @@
+#include "utils.h"
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <pthread.h>
+#include <ctype.h>
+
+struct String {
+    u8 *data {NULL};
+    usize size {0};
+};
+
+struct PDF {
+    String header {};
+};
+
+bool match_string(const String &s1, const char *s2);
+
+void print_string(const String &c);
+void print_comment(const String &c);
+
+PDF parse_pdf(u8 *content, u64 size);
+
+struct Parser {
+    u8 *content {NULL};
+    u64 size {0};
+    u64 curr_byte_indx {0};
+    u8 curr_byte {0};
+
+    bool in_comment {false};
+
+    PDF parsed_pdf{};
+};
+
+inline bool match_string(const String &s1, const char *s2) {
+
+  for (usize i = 0; i < s1.size; i++) {
+    char c = s2[i];
+    if (c == '\0' || c != s1.data[i])
+      return false;
+  }
+
+  return s2[s1.size] == '\0';
+}
+
+
+inline void print_string(const String &c) {
+    printf("%.*s \n", (u32)c.size, c.data); 
+}
+
+inline void print_comment(const String &c) {
+    printf("%.*s", (u32)c.size, c.data); 
+}
+
+inline Parser make_parser(u8 *content, u64 size) {
+  GB_ASSERT_MSG(size, "empty pdf found");
+
+  return Parser{
+      .content = content,
+      .size = size,
+      .curr_byte_indx = 0,
+      .curr_byte = content[0],
+      .parsed_pdf{},
+  };
+}
+
+inline bool next_byte(Parser *p) {
+  if (p->curr_byte_indx < p->size) {
+    p->curr_byte_indx += 1;
+    p->curr_byte = p->content[p->curr_byte_indx];
+    return true;
+  }
+
+  return false;
+}
+
+inline bool next_n_bytes(Parser *p, u64 n) {
+  if (p->curr_byte_indx + n < p->size) {
+    p->curr_byte_indx += n;
+    p->curr_byte = p->content[p->curr_byte_indx];
+    return true;
+  }
+
+  return false;
+}
+
+inline void expect_byte(Parser *p, u8 byte) {
+  GB_ASSERT(p->curr_byte == byte);
+  GB_ASSERT_MSG(next_byte(p), "expect_byte called at last byte");
+}
+
+String parse_comment(Parser *p) {
+  expect_byte(p, '%');
+
+  String c{};
+  c.data = p->content + p->curr_byte_indx;
+
+  u64 start_indx = p->curr_byte_indx;
+
+  while (p->curr_byte != '%') {
+    GB_ASSERT_MSG(next_byte(p), "unclosing comment, reached EOF");
+  }
+
+  c.size = p->curr_byte_indx - start_indx;
+  expect_byte(p, '%');
+
+  if (c.size == 0) {
+    c.data = NULL;
+  }
+
+  return c;
+}
+
+String parse_ansi_string(Parser *p) {
+  GB_ASSERT_MSG(isalpha(p->curr_byte), "expected ascii character");
+
+  String str{};
+  str.data = p->content + p->curr_byte_indx;
+  u64 start_indx = p->curr_byte_indx;
+
+  while (isalpha(p->curr_byte)) {
+    if (!next_byte(p))
+      break;
+  }
+
+  str.size = p->curr_byte_indx - start_indx;
+  return str;
+}
+
+PDF parse_pdf(u8 *content, u64 size) {
+  /* std::string cntnt((char *)content, size); */
+  /* std::cout << cntnt << std::endl; */
+
+  Parser p = make_parser(content, size);
+
+  if (p.curr_byte == '%') {
+    String header = parse_comment(&p);
+    print_comment(header);
+    GB_ASSERT_MSG(next_n_bytes(&p, 9), "invalid pdf header");
+
+    p.parsed_pdf.header = header;
+  }
+
+  for (;;) {
+    if (p.curr_byte == '%') {
+      parse_comment(&p);
+    } else if (isalpha(p.curr_byte)) {
+      String str = parse_ansi_string(&p);
+
+      if (match_string(str, "xref")) {
+      }
+    }
+
+    if (!next_byte(&p))
+      break;
+  }
+
+  return p.parsed_pdf;
+}
+
+
+int main() {
+
+    u8 *content {NULL};
+    u64 size {0};
+    std::ifstream file("../dummy.pdf");
+
+    file.seekg(0, std::ios_base::end);
+    size = file.tellg();
+    content = (u8 *)malloc(size);
+
+    file.seekg(0);
+    file.read((char *)content, size);
+    defer(delete content);
+
+    PDF pdf = parse_pdf(content, size);
+}
+
---
+++ utils.h
@@ -0,0 +1,219 @@
+#ifndef PAPER_TRAIL_UTILS_H
+#define PAPER_TRAIL_UTILS_H
+
+#include <cstddef>
+#include <stdint.h>
+#include <cstdarg>
+#include <stdio.h>
+
+// taken from [https://github.com/gingerBill/gb]
+
+typedef uint8_t   u8;
+typedef  int8_t   i8;
+typedef uint16_t u16;
+typedef  int16_t i16;
+typedef uint32_t u32;
+typedef  int32_t i32;
+typedef uint64_t u64;
+typedef  int64_t i64;
+
+typedef float  f32;
+typedef double f64;
+
+typedef size_t    usize;
+typedef ptrdiff_t isize;
+
+static_assert(sizeof(u8)  == sizeof(i8));
+static_assert(sizeof(u16) == sizeof(i16));
+static_assert(sizeof(u32) == sizeof(i32));
+static_assert(sizeof(u64) == sizeof(i64));
+
+static_assert(sizeof(u8)  == 1);
+static_assert(sizeof(u16) == 2);
+static_assert(sizeof(u32) == 4);
+static_assert(sizeof(u64) == 8);
+
+static_assert(sizeof(f32) == 4);
+static_assert(sizeof(f64) == 8);
+
+static_assert(sizeof(usize) == sizeof(isize));
+
+#define U8_MIN 0u
+#define U8_MAX 0xffu
+#define I8_MIN (-0x7f - 1)
+#define I8_MAX 0x7f
+
+#define U16_MIN 0u
+#define U16_MAX 0xffffu
+#define I16_MIN (-0x7fff - 1)
+#define I16_MAX 0x7fff
+
+#define U32_MIN 0u
+#define U32_MAX 0xffffffffu
+#define I32_MIN (-0x7fffffff - 1)
+#define I32_MAX 0x7fffffff
+
+#define U64_MIN 0ull
+#define U64_MAX 0xffffffffffffffffull
+#define I64_MIN (-0x7fffffffffffffffll - 1)
+#define I64_MAX 0x7fffffffffffffffll
+
+#define F32_MIN 1.17549435e-38f
+#define F32_MAX 3.40282347e+38f
+
+#define F64_MIN 2.2250738585072014e-308
+#define F64_MAX 1.7976931348623157e+308
+
+#undef NULL
+#if defined(__cplusplus)
+    #if __cplusplus >= 201103L
+        #define NULL nullptr
+    #else
+        #define NULL 0
+    #endif
+#else
+    #define NULL ((void *)0)
+#endif
+
+
+////////////////////////////////////////////////////////////////
+//
+// Defer statement
+// Akin to D's SCOPE_EXIT or
+// similar to Go's defer but scope-based
+//
+// NOTE: C++11 (and above) only!
+//
+extern "C++" {
+	template <typename T> struct gbRemoveReference       { typedef T Type; };
+	template <typename T> struct gbRemoveReference<T &>  { typedef T Type; };
+	template <typename T> struct gbRemoveReference<T &&> { typedef T Type; };
+
+	template <typename T> inline T &&gb_forward(typename gbRemoveReference<T>::Type &t)  { return static_cast<T &&>(t); }
+	template <typename T> inline T &&gb_forward(typename gbRemoveReference<T>::Type &&t) { return static_cast<T &&>(t); }
+	template <typename T> inline T &&gb_move   (T &&t)                                   { return static_cast<typename gbRemoveReference<T>::Type &&>(t); }
+	template <typename F>
+	struct gbprivDefer {
+		F f;
+		gbprivDefer(F &&f) : f(gb_forward<F>(f)) {}
+		~gbprivDefer() { f(); }
+	};
+	template <typename F> gbprivDefer<F> gb__defer_func(F &&f) { return gbprivDefer<F>(gb_forward<F>(f)); }
+
+	#define GB_DEFER_1(x, y) x##y
+	#define GB_DEFER_2(x, y) GB_DEFER_1(x, y)
+	#define GB_DEFER_3(x)    GB_DEFER_2(x, __COUNTER__)
+	#define defer(code)      auto GB_DEFER_3(_defer_) = gb__defer_func([&]()->void{code;})
+}
+
+
+////////////////////////////////////////////////////////////////
+//
+// Macro Fun!
+//
+//
+
+#ifndef GB_JOIN_MACROS
+#define GB_JOIN_MACROS
+	#define GB_JOIN2_IND(a, b) a##b
+
+	#define GB_JOIN2(a, b)       GB_JOIN2_IND(a, b)
+	#define GB_JOIN3(a, b, c)    GB_JOIN2(GB_JOIN2(a, b), c)
+	#define GB_JOIN4(a, b, c, d) GB_JOIN2(GB_JOIN2(GB_JOIN2(a, b), c), d)
+#endif
+
+// from [boost/current_function.hpp](https://www.boost.org/doc/libs/1_62_0/boost/current_function.hpp)
+
+#if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600)) || defined(__ghs__)
+
+# define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
+
+#elif defined(__DMC__) && (__DMC__ >= 0x810)
+
+# define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
+
+#elif defined(__FUNCSIG__)
+
+# define BOOST_CURRENT_FUNCTION __FUNCSIG__
+
+#elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
+
+# define BOOST_CURRENT_FUNCTION __FUNCTION__
+
+#elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
+
+# define BOOST_CURRENT_FUNCTION __FUNC__
+
+#elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
+
+# define BOOST_CURRENT_FUNCTION __func__
+
+#elif defined(__cplusplus) && (__cplusplus >= 201103)
+
+# define BOOST_CURRENT_FUNCTION __func__
+
+#else
+
+# define BOOST_CURRENT_FUNCTION "(unknown)"
+
+#endif
+
+
+////////////////////////////////////////////////////////////////
+//
+// Debug
+//
+//
+
+
+#ifndef GB_DEBUG_TRAP
+	#if defined(_MSC_VER)
+	 	#if _MSC_VER < 1300
+		#define GB_DEBUG_TRAP() __asm int 3 /* Trap to debugger! */
+		#else
+		#define GB_DEBUG_TRAP() __debugbreak()
+		#endif
+	#else
+		#define GB_DEBUG_TRAP() abort()
+	#endif
+#endif
+
+#ifndef GB_ASSERT_MSG
+#define GB_ASSERT_MSG(cond, msg, ...) do { \
+	if (!(cond)) { \
+		gb_assert_handler("Assertion Failure", #cond, __FILE__, (i64)__LINE__, msg, ##__VA_ARGS__); \
+		GB_DEBUG_TRAP(); \
+	} \
+} while (0)
+#endif
+
+#ifndef GB_ASSERT
+#define GB_ASSERT(cond) GB_ASSERT_MSG(cond, NULL)
+#endif
+
+#ifndef GB_ASSERT_NOT_NULL
+#define GB_ASSERT_NOT_NULL(ptr) GB_ASSERT_MSG((ptr) != NULL, #ptr " must not be NULL")
+#endif
+
+#ifndef GB_PANIC
+#define GB_PANIC(msg, ...) do { \
+	gb_assert_handler("Panic", NULL, __FILE__, (i64)__LINE__, msg, ##__VA_ARGS__); \
+	GB_DEBUG_TRAP(); \
+} while (0)
+#endif
+
+static void gb_assert_handler(char const *prefix, char const *condition, char const *file, i32 line, char const *msg, ...) {
+	fprintf(stderr, "%s(%d): %s: ", file, line, prefix);
+	if (condition)
+		fprintf(stderr, "`%s` ", condition);
+	if (msg) {
+		va_list va;
+		va_start(va, msg);
+		vfprintf(stderr, msg, va);
+		va_end(va);
+	}
+	fprintf(stderr, "\n");
+}
+
+
+#endif
